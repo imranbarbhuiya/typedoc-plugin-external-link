9eea7c822383a9819f853c42c60101735a7c6ca4
parbez <imranbarbhuiya.fsd@gmail.com>
2022-07-31 19:27:56 +0530

    fix: declare option

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,23 @@
-import { type Application } from 'typedoc';
+import { ParameterType, type Application } from 'typedoc';
 import type { getURL } from './interfaces/config';
-import { resolvePath } from './utils/util';
+import { resolveFilePath, resolvePath } from './utils/util';
 
 export function load(app: Application) {
-	const filePath = resolvePath<{ externalLinkPath?: string }>('typedoc.json')?.externalLinkPath ?? 'externalConfig.js';
+	app.options.addDeclaration({
+		name: 'externalLinkPath',
+		help: 'Define the path to the external links config file',
+		type: ParameterType.String,
+		defaultValue: 'externalConfig.js'
+	});
+
+	// this option doesn't work so manually reading the file
+	const filePath =
+		resolvePath<{ externalLinkPath?: string }>('typedoc.json')?.externalLinkPath ?? (app.options.getValue('externalLinkPath') as string);
 
 	const config = resolvePath<{ packageNames: string[]; getURL: getURL }>(filePath);
 
 	if (!config) {
-		return app.logger.error(`External links config file \`${filePath}\` not found`);
+		return app.logger.error(`External links config file \`${resolveFilePath(filePath)}\` not found`);
 	}
 
 	const { packageNames, getURL } = config;
--- src/utils/util.ts
+++ src/utils/util.ts
@@ -1,8 +1,12 @@
 import { join } from 'node:path';
 
+export function resolveFilePath(filePath: string): string {
+	return join(process.cwd(), filePath);
+}
+
 export function resolvePath<T>(path: string): T | null {
 	try {
-		return require(join(process.cwd(), path));
+		return require(resolveFilePath(path));
 	} catch {
 		return null;
 	}
